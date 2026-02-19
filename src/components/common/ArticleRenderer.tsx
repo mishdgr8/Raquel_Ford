c742a65c39be55e4c16cd9253522ee823459d493
mishdgr8 <Mishaelmordi@gmail.com>
2026-02-19 16:22:09 +0100

    fix(embeds): Use official Instagram blockquote embed code

--- src/components/common/ArticleRenderer.tsx
+++ src/components/common/ArticleRenderer.tsx
@@ -83,14 +83,41 @@ export function ArticleRenderer({ blocks, html }: ArticleRendererProps) {
                         }
                         if (block.data.embedType === 'instagram' && block.data.embedId) {
                             return (
-                                <div key={block.id} style={{ margin: '2rem 0', textAlign: 'center' }}>
-                                    <iframe
-                                        src={`https://www.instagram.com/p/${block.data.embedId}/embed/captioned`}
-                                        style={{ width: '100%', maxWidth: '540px', height: '600px', border: 0 }}
-                                        scrolling="no"
-                                        loading="lazy"
-                                        title="Instagram embed"
-                                    />
+                                <div key={block.id} style={{ margin: '2rem 0', display: 'flex', justifyContent: 'center' }}>
+                                    <blockquote
+                                        className="instagram-media"
+                                        data-instgrm-permalink={`https://www.instagram.com/p/${block.data.embedId}/`}
+                                        data-instgrm-version="14"
+                                        style={{
+                                            background: '#FFF',
+                                            border: '0',
+                                            borderRadius: '3px',
+                                            boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
+                                            margin: '1px',
+                                            maxWidth: '540px',
+                                            minWidth: '326px',
+                                            padding: '0',
+                                            width: 'calc(100% - 2px)'
+                                        }}
+                                    >
+                                        <div style={{ padding: '16px' }}>
+                                            <a
+                                                href={`https://www.instagram.com/p/${block.data.embedId}/`}
+                                                style={{
+                                                    background: '#FFFFFF',
+                                                    lineHeight: '0',
+                                                    padding: '0 0',
+                                                    textAlign: 'center',
+                                                    textDecoration: 'none',
+                                                    width: '100%'
+                                                }}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                View this post on Instagram
+                                            </a>
+                                        </div>
+                                    </blockquote>
                                 </div>
                             );
                         }
