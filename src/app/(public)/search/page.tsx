ec1d8c22c5146611cafce40f8368dc877aab91a0
mishdgr8 <Mishaelmordi@gmail.com>
2026-02-20 11:13:32 +0100

    Fix build error: remove revalidate from Client Component search/page.tsx

--- src/app/(public)/search/page.tsx
+++ src/app/(public)/search/page.tsx
@@ -10,8 +10,6 @@ import { PostCard } from "@/components/blocks/PostCard";
 import styles from "./SearchPage.module.css";
 import { Suspense } from "react";
 
-export const revalidate = 60;
-
 function SearchResults() {
     const searchParams = useSearchParams();
     const query = searchParams.get('q');
